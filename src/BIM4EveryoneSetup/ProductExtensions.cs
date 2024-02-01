5e61f1db946f19f7bf879562b6ba3682392cf0fc
dosymep <biseuv@yandex.ru>
2024-02-01 13:55:13 +0300

    Исправил конфигурирование pyRevit

--- src/BIM4EveryoneSetup/ProductExtensions.cs
+++ src/BIM4EveryoneSetup/ProductExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Text;
 
@@ -6,6 +7,8 @@ using WixSharp.CommonTasks;
 
 namespace BIM4EveryoneSetup {
     internal static class ProductExtensions {
+        private static int _configActionNameId = 0;
+        
         public static T SetProductInfo<T>(this T self) where T : Project {
             self.GUID = Constants.ProductId;
             self.UpgradeCode = Constants.ProductId;
@@ -173,72 +176,100 @@ namespace BIM4EveryoneSetup {
 
         private static void CreateConfigureUpdateExtensions<T>(this T self) where T : Project {
             string[] args = new[] {
-                "extensions paths forget --all",
+                "extensions paths forget --all", 
                 "extensions paths add \"%appdata%\\pyRevit\\Extensions\"",
                 "extensions update --all",
             };
 
-            args.ForEach(item => 
+            args.ForEach(item =>
                 self.CreateConfigureAction(item, "Обновление установленных расширений"));
         }
-        
+
         private static void CreateConfigureDefaultParams<T>(this T self) where T : Project {
             string[] args = new[] {
-                "configs core:user_locale ru", 
-                "configs rocketmode enable", 
+                "configs core:user_locale ru",
+                "configs rocketmode enable",
                 "configs autoupdate enable",
-                "configs checkupdates enable",
-                "configs usercanextend yes",
+                "configs checkupdates enable", 
+                "configs usercanextend yes", 
                 "configs usercanconfig yes",
             };
 
-            args.ForEach(item => 
+            args.ForEach(item =>
                 self.CreateConfigureAction(item, "Настройка дефолтных параметров"));
         }
-        
+
         private static void CreateConfigureDisableBuiltinExtensions<T>(this T self) where T : Project {
             string[] args = new[] {
-                "extensions disable pyRevitBundlesCreatorExtension.extension",
-                "extensions disable pyRevitCore.extension",
-                "extensions disable pyRevitDevHooks.extension",
-                "extensions disable pyRevitDevTools.extension",
-                "extensions disable pyRevitTags.extension",
-                "extensions disable pyRevitTemplates.extension",
-                "extensions disable pyRevitTools.extension",
-                "extensions disable pyRevitTutor.extension",
+                "configs pyRevitBundlesCreatorExtension.extension:disabled true",
+                "configs pyRevitBundlesCreatorExtension.extension:private_repo true",
+                "configs pyRevitBundlesCreatorExtension.extension:username \"\"",
+                "configs pyRevitBundlesCreatorExtension.extension:password \"\"",
+                "configs pyRevitCore.extension:disabled true", 
+                "configs pyRevitCore.extension:private_repo true",
+                "configs pyRevitCore.extension:username \"\"",
+                "configs pyRevitCore.extension:password \"\"",
+                "configs pyRevitDevHooks.extension:disabled true",
+                "configs pyRevitDevHooks.extension:private_repo true",
+                "configs pyRevitDevHooks.extension:username \"\"", 
+                "configs pyRevitDevHooks.extension:password \"\"",
+                "configs pyRevitDevTools.extension:disabled true",
+                "configs pyRevitDevTools.extension:private_repo true",
+                "configs pyRevitDevTools.extension:username \"\"",
+                "configs pyRevitDevTools.extension:password \"\"",
+                "configs pyRevitTags.extension:disabled true", 
+                "configs pyRevitTags.extension:private_repo true",
+                "configs pyRevitTags.extension:username \"\"", 
+                "configs pyRevitTags.extension:password \"\"",
+                "configs pyRevitTemplates.extension:disabled true",
+                "configs pyRevitTemplates.extension:private_repo true",
+                "configs pyRevitTemplates.extension:username \"\"", 
+                "configs pyRevitTemplates.extension:password \"\"",
+                "configs pyRevitTools.extension:disabled true", 
+                "configs pyRevitTools.extension:private_repo true",
+                "configs pyRevitTools.extension:username \"\"",
+                "configs pyRevitTools.extension:password \"\"",
+                "configs pyRevitTutor.extension:disabled true",
+                "configs pyRevitTutor.extension:private_repo true",
+                "configs pyRevitTutor.extension:username \"\"",
+                "configs pyRevitTutor.extension:password \"\"",
             };
 
             args.ForEach(item =>
                 self.CreateConfigureAction(item, "Отключение встроенных расширений pyRevit"));
         }
-        
+
         private static void CreateAttachRevits<T>(this T self) where T : Project {
             string[] args = new[] {
                 "detach --all",
-                "attach master 277 2020",
+                "attach master 277 2020", 
                 "attach master 277 2021",
                 "attach master 277 2022",
                 "attach master 277 2023",
                 "attach master 277 2024",
             };
 
-             args.ForEach(item => 
-                 self.CreateConfigureAction(item, "Прикрепление поддерживаемых версий Revit"));
+            args.ForEach(item =>
+                self.CreateConfigureAction(item, "Прикрепление поддерживаемых версий Revit"));
         }
 
         private static void CreateConfigureAction<T>(this T self,
             string args, string message, Condition condition = null) where T : Project {
             condition = condition ?? Constants.ConfigInstallCondition;
 
-            Action action = new WixQuietExecAction(
+            WixQuietExecAction action = new WixQuietExecAction(
                 Constants.pyRevitCliPath, args,
-                Return.check, When.After, Step.InstallFinalize, condition);
+                Return.check, When.After, Step.InstallFinalize, condition) {Id = GenerateActionId("_ConfigureAction_")};
 
             self.AddAction(action);
             self.AddXmlElement(
                 "Wix/Package/UI",
                 "ProgressText",
                 $"Action={action.Id};Message={message}");
+        }
+
+        private static string GenerateActionId(string name) {
+            return $"{name}.{_configActionNameId++}";
         }
     }
 }
